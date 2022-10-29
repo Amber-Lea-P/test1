1cb13d1f64750f00b374f14b65e0c0e131bc61b9
Amber-Lea-P <amber.pontinen@gmail.com>
2022-10-29 10:54:43 +0300

    update to profile.html, creatition of test.js

---
+++ Javascript/test.js
@@ -0,0 +1,14 @@
+var a = 10;
+// var: variable : is is the memory location where can store the value.
+// 10 + 20 : 10 value and 20 value and result as well.
+// memory +
+var b = 20;
+
+
+a = "abhi";
+b = true; //true : yes ,  false : no: boolean value
+
+var c = a + b;
+
+console.log("hello from Amber");
+console.log("result is" +c);
